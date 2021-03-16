1b7f9bdd5720b13c7841f4f0bf949fb9e3159cac
pinkninjajess <jessica.valenti1@gmail.com>
2021-03-16 10:48:04 -0400

    Lab 13 - added properties to the lib schema

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -2,7 +2,15 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (host: Tree, schema: any) {
-  await libraryGenerator(host, { name: schema.name });
+  schema.linter = 'tslint';
+  schema.name = 'util-' + schema.name;
+  schema.directory = schema.directory;
+
+  await libraryGenerator(host,
+  { name: schema.name,
+  linter: schema.linter,
+  directory: schema.directory,
+  tags: `type:util, scope:${schema.directory}`);
   await formatFiles(host);
   return () => {
     installPackagesTask(host);
