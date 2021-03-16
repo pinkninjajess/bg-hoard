86f221968e1bf4f5ebb0796d5d706f1542befb76
pinkninjajess <jessica.valenti1@gmail.com>
2021-03-16 11:33:30 -0400

    Lab 14 - modified sort-project-references generator

--- tools/generators/sort-project-references/index.ts
+++ tools/generators/sort-project-references/index.ts
@@ -1,10 +1,26 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+import { Tree, formatFiles, installPackagesTask, updateJson } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (host: Tree, schema: any) {
-  await libraryGenerator(host, { name: schema.name });
+function sortKeys(host: Tree, file: string) {
+  updateJson(host, file, (json) => {
+    json.projects = sortObjectKeys(json.projects);
+    return json;
+  });
+}
+
+function sortObjectKeys(obj: any) {
+  const sorted = {};
+  Object.keys(obj)
+    .sort()
+    .forEach((key) => {
+      sorted[key] = obj[key];
+    });
+  return sorted;
+}
+
+export default async function(host: Tree) {
+  sortKeys(host, 'workspace.json');
+  sortKeys(host, 'nx.json');
   await formatFiles(host);
-  return () => {
-    installPackagesTask(host);
-  };
+}
 }
